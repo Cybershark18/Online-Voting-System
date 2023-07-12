616825623d9416bccbc2333363298197e5c732fb
Cybershark18 <bandi.shiva2020@vitbhopal.ac.in>
2023-07-12 22:04:05 +0530

    commit

---
+++ online-voting-system-main/votingSystemLaravel-master/resources/views/welcome.blade.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>HomePage</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="{{ URL::asset('/asset/home/assets/img/favicon.png')}}" rel="icon">
+  <link href="{{ URL::asset('/asset/home/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700|Raleway:300,400,400i,500,500i,700,800,900" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{ URL::asset('/asset/home/assets/vendor/animate.css/animate.min.css')}}" rel="stylesheet">
+  <link href="{{ URL::asset('/asset/home/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{ URL::asset('/asset/home/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{ URL::asset('/asset/home/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+  <link href="{{ URL::asset('/asset/home/assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+  <link href="{{ URL::asset('/asset/home/assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{{ URL::asset('/asset/home/assets/css/style.css')}}" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex justify-content-between">
+
+      <div class="logo">
+        <h1><a href="index.html"><span>Voting</span>System</a></h1>
+        
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+       
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li class="dropdown"><a href="#"><span>LogIn</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="/voterLogin">Login Voter</a></li>
+              <li><a href="/login">Login Candidate</a></li>
+              <li><a href="/commissionerLogin">Login Commissioner</a></li>
+   
+              <li class="dropdown"><a href="#"><span>registration</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="/voterSignUp">Voter</a></li>
+                  <li><a href="/register">Candidate</a></li>
+                 
+                </ul>
+              </li>
+          
+        
+         
+             
+              
+            </ul>
+            <li><a href="#blog">Partner</a></li>
+          </li>
+
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= hero Section ======= -->
+  
+
+    <!-- ======= About Section ======= -->
+    <div id="about" class="about-area area-padding">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="section-headline text-center">
+              <h2>About Voting System</h2>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <!-- single-well start-->
+          <div class="col-md-6 col-sm-6 col-xs-12">
+            <div class="well-left">
+              <div class="single-well">
+                <a href="#">
+                  <img src="asset/home/assets/img/about/1.jpg" alt="">
+                </a>
+              </div>
+            </div>
+          </div>
+          <!-- single-well end-->
+          <div class="col-md-6 col-sm-6 col-xs-12">
+            <div class="well-middle">
+              <div class="single-well">
+                <a href="#">
+                  <h4 class="sec-head">voting System Management</h4>
+                </a>
+                <p>
+                    An online voting system is a software platform that allows groups to securely conduct votes and elections. High-quality online voting systems balance ballot security, accessibility and the overall requirements of an organization's voting event
+                </p>
+                <ul>
+                  <li>
+                    <i class="bi bi-check"></i> Cost Savings and Efficiency
+                  </li>
+                  <li>
+                    <i class="bi bi-check"></i> Voter Accessibility
+                  </li>
+                  <li>
+                    <i class="bi bi-check"></i> Auditability and Verifiability
+                  </li>
+                  <li>
+                    <i class="bi bi-check"></i> Security, Confidence, and Trust in Election Results
+                  </li>
+                  <li>
+                    <i class="bi bi-check"></i> Organizational leader
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <!-- End col-->
+        </div>
+      </div>
+    </div><!-- End About Section -->
+
+  >
+    <div class="suscribe-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs=12">
+            <div class="suscribe-text text-center">
+              <h3>Welcome to our Online Voting System</h3>
+              <a class="sus-btn" href="#">Login</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div><!-- End Suscribe Section -->
+
+    <!-- ======= Contact Section ======= -->
+   
+  <!-- ======= Footer ======= -->
+  <footer>
+    <div class="footer-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="footer-content">
+              <div class="footer-head">
+                <div class="footer-logo">
+                  <h2><span>Voting</span>System</h2>
+                </div>
+
+                <p>Our online election security measures protect you, your data, and your organization against risk</p>
+                <div class="footer-icons">
+                  <ul>
+                    <li>
+                      <a href="#"><i class="bi bi-facebook"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="bi bi-twitter"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="bi bi-instagram"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="bi bi-linkedin"></i></a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- end single footer -->
+          <div class="col-md-4">
+            <div class="footer-content">
+              <div class="footer-head">
+                <h4>information</h4>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas quas asperiores nemo, necessitatibus 
+                </p>
+                <div class="footer-contacts">
+                  <p><span>Tel:</span> 5424243545</p>
+                  <p><span>Email:</span> votingsystem@gmail.com</p>
+                  <p><span>Working Hours:</span> 9am-8pm</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- end single footer -->
+      
+    <div class="footer-area-bottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="copyright text-center">
+            
+            </div>
+            <div class="credits">
+              
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="{{ URL::asset('/asset/home/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{ URL::asset('/asset/home/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+  <script src="{{ URL::asset('/asset/home/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <script src="{{ URL::asset('/asset/home/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+  <script src="{{ URL::asset('/asset/home/assets/vendor/php-email-form/validate.js')}}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{ URL::asset('/asset/home/assets/js/main.js')}}"></script>
+
+</body>
+
+</html>
